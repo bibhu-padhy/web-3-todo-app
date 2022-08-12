2d74fbe9969b92433d39177c1cf4063dc6188bf6
bibhu-padhy <bibhupadhy140@gmail.com>
2022-08-12 07:46:48 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-3xl font-bold underline">Web 3 Todo App</div>;
+}
+
+export default App;
